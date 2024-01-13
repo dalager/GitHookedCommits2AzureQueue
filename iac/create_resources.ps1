f4793a68b44a7b24d97201b57243cee735f66032
Christian Dalager <christian.dalager@eksponent.com>
2024-01-13 10:40:29 +0100

    update createscript

--- iac/create_resources.ps1
+++ iac/create_resources.ps1
@@ -3,8 +3,7 @@ $resourcegroup = "commitlogger-rg"
 $location = "westeurope"
 $storageaccount = "commitloggerstorage$random"
 $storagequeue = "commitqueue"
-$commitloggerdb = "commitloggerdb$random"
-
+$envvariable = "COMMITLOGGER_QUEUE_URL"
 # Create resource group
 az group create --name $resourcegroup --location $location
 
@@ -18,12 +17,17 @@ az storage queue create --name $storagequeue --account-name $storageaccount
 az storage queue policy create --account-name $storageaccount --queue-name $storagequeue --name "addcommits" --permissions "a" --expiry "2028-12-31T23:59:00Z"
 
 # create a sas token for pushing messages
-$token = az storage queue generate-sas --account-name $storageaccount -o tsv --policy-name "addcommits"
+$token = az storage queue generate-sas --account-name $storageaccount --name $storagequeue --policy-name "addcommits" -o tsv 
+
+Write-Output "SAS token for pushing messages:`n $token"
+
+$storageendpoint = az storage account show --name $storageaccount --resource-group $resourcegroup --query "primaryEndpoints.queue" -o tsv
+
+$queueposturl = "$storageendpoint$storagequeue/messages?$token"
+write-output "This url can be used for posting messages to the queue for the next 5 years:`n $queueposturl"
+
+Write-Output "saving to environment variable $envvariable"
+# add as environment variable
+[Environment]::SetEnvironmentVariable("$envvariable", $queueposturl, "User")
 
 
-
-
-
-
-# create cosmosdb
-az cosmosdb create -n commitloggerdb$random -g $RESOURCE_GROUP --enable-free-tier true
