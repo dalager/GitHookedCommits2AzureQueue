8bbca38807f204a5279020c2c6890263aec0165a
Christian Dalager <christian.dalager@eksponent.com>
2024-01-13 09:46:26 +0100

    hook update

---
+++ hook/install.ps1
@@ -0,0 +1,25 @@
+# install post commit hook
+
+$ErrorActionPreference = "Stop"
+$installpath = "$HOME\.git-hooks"
+$sourcepath = ".\post-commit"
+$sourcename = "post-commit"
+if (!(Test-Path $installpath)) {
+    New-Item -ItemType Directory -Force -Path $installpath | Out-Null
+}
+if (!(Test-Path $installpath)) {
+    throw "Could not create directory $installpath"
+    exit
+}
+
+Copy-Item $sourcepath $installpath -Force
+
+if (Test-Path $installpath/$sourcename) {
+    Write-Host "Successfully installed post-commit hook as $installpath\$sourcename"
+}
+else {
+    throw "Could not install post-commit hook"
+    exit
+}
+
+git config core.hooksPath $installpath
